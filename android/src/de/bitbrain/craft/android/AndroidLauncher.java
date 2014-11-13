3cac143d0f0d6a58151a8db351d2381ff12761b9
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2014-11-13 03:25:58 +0100

    Moved to LibGDX 1.4.x

--- android/src/de/bitbrain/craft/android/AndroidLauncher.java
+++ android/src/de/bitbrain/craft/android/AndroidLauncher.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
 import de.bitbrain.craft.CraftGame;
+import de.bitbrain.craft.inject.SharedInjector;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new CraftGame(), config);
+		initialize(SharedInjector.get().getInstance(CraftGame.class), config);
 	}
 }
