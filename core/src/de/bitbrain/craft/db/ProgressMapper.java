d906faf8b5a5c3ea6c5629f72c225d5521cd662a
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-01-01 21:52:56 +0100

    Added missing javadoc

--- core/src/de/bitbrain/craft/db/ProgressMapper.java
+++ core/src/de/bitbrain/craft/db/ProgressMapper.java
@@ -1,3 +1,22 @@
+/*
+ * Craft - Crafting game for Android, PC and Browser.
+ * Copyright (C) 2014 Miguel Gonzalez
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package de.bitbrain.craft.db;
 
 import java.util.Collection;
@@ -10,6 +29,13 @@ import de.bitbrain.jpersis.annotations.Mapper;
 import de.bitbrain.jpersis.annotations.Select;
 import de.bitbrain.jpersis.annotations.Update;
 
+/**
+ * Data mapper for {@link de.bitbrain.craft.models.Progress}
+ *
+ * @author Miguel Gonzalez <miguel-gonzalez@gmx.de>
+ * @since 1.0
+ * @version 1.0
+ */
 @Mapper("de.bitbrain.craft.models.Progress")
 public interface ProgressMapper {
 	
