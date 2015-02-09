29fe9acb55216fc743831dc16198c0377b8913df
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-02-09 21:33:07 +0100

    Removed unneeded dependency

--- core/src/de/bitbrain/craft/graphics/ScreenFader.java
+++ core/src/de/bitbrain/craft/graphics/ScreenFader.java
@@ -25,7 +25,6 @@ import aurelienribon.tweenengine.TweenCallback;
 import aurelienribon.tweenengine.TweenEquations;
 import aurelienribon.tweenengine.TweenManager;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
